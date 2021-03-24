0b1bc4e1869f83d4385c82043415e82777fd9cd3
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-03-25 00:43:37 +0200

    Выполнил третье задание - работает достаточно костыльно, но всё +- работает, буду рад любым замечаниям.

---
+++ Task3/station.rb
@@ -0,0 +1,24 @@
+class Station
+  attr_accessor :station_name, :list_trains, :listed_trains
+
+  def station_info
+    puts 'Введите название вашей главной станции'
+    @station_name = gets
+    puts "Ваша главная станция - #{@station_name}"
+  end
+
+  def initialize(list_trains = nil)
+    @list_trains = list_trains
+    @listed_trains = list_trains
+  end
+
+  def train_list(train_num)
+    @list_trains = []
+    @list_trains.append(train_num)
+    listed_trains = @list_trains[0]
+    puts "Номера поездов на станции #{@station_name}: #{listed_trains}"
+  end
+
+end
+
+
