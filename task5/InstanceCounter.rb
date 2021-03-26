9bcfed4f0ca27b6c3a609f695ca9287ab1a89d91
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-03-26 13:18:07 +0200

    Выполнил задание с InstanceCounter

--- task5/InstanceCounter.rb
+++ task5/InstanceCounter.rb
@@ -1,1 +1,9 @@
-# Testing
+protected_methods(InstanceCounter)
+module InstanceCounter
+
+  def self.count_instances
+    ObjectSpace.garbage_collect
+    ObjectSpace.each_object(self).to_a.size
+  end
+
+end
